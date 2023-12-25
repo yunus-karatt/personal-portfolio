aa2e147f300d917d9454a99123d65c0078f95285
yunus <yyas404@gmail.com>
2023-12-25 08:51:14 +0530

    responsive navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,30 @@
-import React from "react";
-import styles from './Navbar.module.css'
+import React, { useState } from "react";
+import styles from "./Navbar.module.css";
+import { getImageUrl } from "../../utils";
+
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className={styles.navbar}>
-      <a className={styles.title} href="/">Portfolio</a>
+      <a className={styles.title} href="/">
+        Portfolio
+      </a>
       <div className={styles.menu}>
-        <ul className={styles.menuItems}>
+        <img
+          className={styles.menuBtn}
+          src={
+            menuOpen
+              ? getImageUrl("nav/closeIcon.png")
+              : getImageUrl("nav/menuIcon.png")
+          }
+          alt="menu-button"
+          onClick={() => setMenuOpen((prevState) => !prevState)}
+        />
+        <ul
+          className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+          onClick={()=>setMenuOpen(false)}
+        >
           <li>
             <a href="#about">About</a>
           </li>
