af7371c38affd259e12754447399990f03cdadd1
yunus <yyas404@gmail.com>
2023-12-25 09:30:26 +0530

    hero section

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { getImageUrl } from '../../utils'
+import styles from './Hero.module.css'
+
+const Hero = () => {
+  return (
+    <section className={styles.container} >
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Yunus Karatt</h1>
+        <p className={styles.description}>I'm a full-stack developer. Reach out if you'd like to learn more!</p>
+        <a className={styles.contactBtn} href="mailto:yyas404@gmail.com">Contact Me</a>
+      </div>
+        <img className={styles.heroImg} src={getImageUrl("hero/heroImage.png")} alt="Hero image" />
+        <div className={styles.topBlur}/>
+        <div className={styles.bottomBlur}/>
+    </section>
+  )
+}
+
+export default Hero
